1a1faccce4d92082d74506517d2675404ed0dd4e
Corey Willis <coreydwillis@gmail.com>
2025-04-20 06:19:20 -0600

    Egg Pickups
    - Created Egg Pickup variants that we can spawn randomly or triggered by specific rabbits.
    - Imported objects graphics

---
+++ scenes/game/objects/egg_pickup.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+class_name EggPickup
+
+@onready var egg_graphic = $EggGraphic
+
+
+@export var egg_type : String
+@export var egg_value : int
+@export var egg_type_graphic : CompressedTexture2D
+
+func _ready():
+	egg_graphic.texture = egg_type_graphic
+
+func _on_area_2d_body_entered(body):
+	Game.eggs += egg_value
+	SignalBus.egg_count_change.emit()
+	queue_free()
