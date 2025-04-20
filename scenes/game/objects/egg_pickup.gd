4f34ba68edb39599793ca553d9cfdf32496296ae
Corey Willis <coreydwillis@gmail.com>
2025-04-20 10:57:07 -0600

    Updated Sun/Moon Graphics
    Replaced placeholder assets with ones by Monica

--- scenes/game/objects/egg_pickup.gd
+++ scenes/game/objects/egg_pickup.gd
@@ -12,7 +12,7 @@ class_name EggPickup
 func _ready():
 	egg_graphic.texture = egg_type_graphic
 
-func _on_area_2d_body_entered(body):
+func _on_area_2d_body_entered(_body):
 	Game.eggs += egg_value
 	SignalBus.egg_count_change.emit()
 	queue_free()
